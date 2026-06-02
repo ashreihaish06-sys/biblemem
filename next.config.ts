f1851c771d5821a2b53361aa6f955f2b0fa9aff8
ashreihaish06-sys <ashreihaish06@gmail.com>
2026-06-02 10:59:40 +0900

    Add files via upload

--- next.config.ts
+++ next.config.ts
@@ -1,15 +1,7 @@
-import withPWAInit from "@ducanh2912/next-pwa";
 import type { NextConfig } from "next";
-
-const withPWA = withPWAInit({
-  dest: "public",
-  disable: process.env.NODE_ENV === "development",
-  register: true,
-  skipWaiting: true,
-});
 
 const nextConfig: NextConfig = {
   /* config options here */
 };
 
-export default withPWA(nextConfig);
+export default nextConfig;
